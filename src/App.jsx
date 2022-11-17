de0dc98b7c502e1962f3cc5e652d9ceda056cb43
y-enei <43557939+y-enei@users.noreply.github.com>
2022-11-18 08:38:30 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColofulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>Hello!</h1>
+      <ColorfulMessage color="blue">お元気ですか？！</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です！！！</ColorfulMessage>
+      <button onClick={onClickButton}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^o^)</p>}
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hello!</h1>
+//       <p>ハロー</p>
+//     </>
+//   );
+// };
+
+ReactDom.render(<App />, document.getElementById("root"));
